0fd31b037bc00faf7e529e8ff60792f2dc1a3081
Edgar Martinez <96887405+papichulo2021@users.noreply.github.com>
2025-03-16 21:14:17 -0700

    Collection Page

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import HeroCarousel from '../components/Hero/HeroCarousel'
+import React, { useContext, useState } from 'react'
+import {ShopContext} from '../context/ShopContext'
 
 function Collection() {
+  const { products }  = useContext(ShopContext);
+  const [isTrousersOpen, setIsTrouserOpen] = useState(false)
+  
+
+  const [showFilter, setShowFilter] = useState(false);
+
   return (
-    <div>
+    <div className='px-10 flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t '>
+      
+      {/* Filter Option */}
+      <div className='min-w-60'>
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>Category</p>
+          {/* Category filter */}
+          <div className='border border-gray-300 pl-5 py-3 mt-6'>
+
+          </div>
+      </div>
+
     </div>
   )
 }
