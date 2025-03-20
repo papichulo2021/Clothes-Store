37073a1c4a180ee269bd2f0cbc1164beb87c042a
Edgar Martinez <96887405+papichulo2021@users.noreply.github.com>
2025-03-19 22:39:27 -0700

    cart component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import {ShopContext} from '../context/ShopContext'
 
 function Cart() {
+  const {products, currency, cartItems} = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(()=>{
+    
+  },[])
+
   return (
     <div>
       Cart
